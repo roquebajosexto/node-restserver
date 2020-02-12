275d2f6c8c82d4d0601a2a51c7e67765a98d2f98
Roquebajosexto <roquebajosexto@gmail.com>
2020-02-11 21:54:25 -0600

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,6 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://roquebajosexto:XF1ng6nsSJigVOyZ@cluster0-luugy.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI
 }
 process.env.URLDB = urlDB
